e0ad62b755902027816796903c7a9b0d51a92f41
sankalp <sankalp2924@gmail.com>
2025-06-09 20:23:42 +0530

    dep-fix 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from "vite-plugin-static-copy";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "node_modules/@ffmpeg/core/dist/ffmpeg-core.js",
-          dest: "public",
-        },
-        {
-          src: "node_modules/@ffmpeg/core/dist/ffmpeg-core.wasm",
-          dest: "public",
-        },
-      ],
-    }),
-  ],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
